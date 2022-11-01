faa9273c770e95254f36fa07ed1fb6231cb40769
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-11-01 13:45:56 +0100

    started Angular UI for employees

---
+++ IT Provisioning API/ITProvisioning.API/ITProvisioning.API/Data/ITProvisioningDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ITProvisioning.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITProvisioning.API.Data {
+    public class ITProvisioningDbContext : DbContext {
+        public ITProvisioningDbContext(DbContextOptions options) : base(options) {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
