555222d5a0a3854f4061c4ff76bb5c75277b2d3e
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-11-13 11:00:32 +0100

    added gitignore

--- IT Provisioning API/ITProvisioning.API/ITProvisioning.API/Controllers/EmployeesController.cs
+++ IT Provisioning API/ITProvisioning.API/ITProvisioning.API/Controllers/EmployeesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ITProvisioning.API.Controllers {
+    
     [ApiController]
     [Route("api/[controller]")]
     public class EmployeesController : Controller {
